c13985e899a41c6b147defa81ca2a70bc5eea6d4
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-23 13:50:47 -0500

    Added reactstrap and component for images

---
+++ src/components/Images.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ class Images extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Images Will be Here</h1>
+      </div>
+    )
+  }
+}
+ export default Images;
