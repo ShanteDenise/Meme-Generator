b10f76761d12921a291d3058e16d0b4eab4eb704
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-23 15:18:30 -0500

    Added state to images component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Home Screen</h1>
       <Images/>
       </div>
     );
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
+const initialState = {
+    toptext: "",
+    bottomtest: "",
+    isTopDragging: false,
+    isBottomDragging: false,
+    topY: "10%",
+    topX: "50%",
+    bottomX: "50%",
+    bottomY: "90%"
+}
+
  class Images extends React.Component {
+         state = {
+             currentImage: 0,
+             modalIsOpen: false,
+             ...initialState
+         };
+     
   render() {
     return (
       <div>
