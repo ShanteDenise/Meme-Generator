e5cb1a6661e91edbf77d3bbe5b5eda43e73760a7
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-23 16:09:46 -0500

    Looped through array of photos using map and rendered to page

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,4 +1,17 @@
 import React from 'react'
+import {Modal, ModalHeader, ModalBody, FormGroup, Label, NavbarBrand} from 'reactstrap';
+
+const photos = [
+    { src: 'img/crying-face-dawson.jpg'},
+    { src: 'img/First-World-Problems.jpg'},
+    {src: 'img/mad-arthur.jpg'},
+    {src: 'img/side-eye-chloe.jpg'},
+    {src: 'img/Successful_kid_meme.png'},
+    {src: 'img/Think-About-It.jpg'},
+    {src: 'img/Unimpressed_kid.jpg'},
+    {src: 'img/Everywhere-ToyStory'}
+
+]
 
 const initialState = {
     toptext: "",
@@ -21,7 +34,22 @@ const initialState = {
   render() {
     return (
       <div>
-          <h1>Images Will be Here</h1>
+          <div className="content"></div>
+          {photos.map((image, index) => (
+          <div className="image" key={image.src}>
+            <span className="topCaption">Top text</span>
+            <img
+              style={{
+                width: "100%",
+                height: "100%"
+              }}
+              alt={index}
+              src={image.src}
+              onClick={() => this.openImage(index) /* The onclick here determines current image */}
+            />
+            <span className="bottomCaption">Bottom text</span>
+          </div>
+        ))}
       </div>
     )
   }
