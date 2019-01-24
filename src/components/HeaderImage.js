d75cc702ec92bd9daed17a8f8648aae88eb4f879
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-23 23:01:56 -0500

    Added a header component

---
+++ src/components/HeaderImage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+          <div>
+              <h1 className="title-container">Click On An Image</h1>
+              <p className="title-container">Make A Meme All Your Friends Will Love!</p>
+          </div>
+        
+      </div>
+    )
+  }
+}
+
+export default Header
